3d4ca58c6458648b53ca2f244adeaf62b08786b2
William A. Burgess <silvusvalentine@hotmail.com>
2015-05-06 21:29:52 -0400

    Reworked layout first draft

--- js/app.js
+++ js/app.js
@@ -1,63 +1,78 @@
 jQuery( document ).ready(function() {
 
-  var Editor = (function( $ ) {
+  editor = $( ".input-container" );
+  input = $( ".input-field" );
+  lines = $( ".output-line-numbers" );
+  output = $( ".output-field" );
 
-    var input, lines, self, settings;
+  input.on("keyup", sendToViewer);
 
-    var defaults = {
-      input: {
-        class: "editor-input",
-        contenteditable: "true"
-      },
-      lines: {
-        class: "editor-lines"
-      }
-    };
-
-    var directFocus = function() {
-      input.focus();
-    };
-
-    var inputChanged = function( e ) {
-      e.preventDefault();
-      input.append(String.fromCharCode(e.which));
-    };
-
-    var insertNewLine = function() {
-      input.append("\n");
-    };
-
-    var bindEvents = function() {
-      self.on("click", directFocus);
-      input.on("keypress", inputChanged);
-    };
-
-    var init = function( options ) {
-      settings = $.extend( true, {}, defaults, options );
-
-      self = $( "#js-text-editor" );
-
-      if( !settings.lines.hidden ) {
-        lines = $( "<div />" )
-        .attr( settings.lines )
-        .appendTo( self );
-      }
-
-      input = $( "<div />" )
-      .attr( settings.input )
-      .appendTo( self );
-
-      bindEvents();
-    };
-
-    return {
-      init: init
-    };
-
-  })( jQuery );
-
-
-  Editor.init();
-
+  function sendToViewer(e) {
+    var text = input.val();
+    output.text(text);
+    lines.text("");
+    var lineCount = output.text().split(/\n/g).length;
+    for(var i = 1; i < lineCount + 1; i++) {
+      lines.append(i + "\n");
+    }
+    // for(var i = 1; i < output.text().split(/\n/g).length + 3; i++) {
+    //   lines.append(i + "\n");
+    // }
+  }
 
 });
+
+
+// var Editor = (function( $ ) {
+
+  //   var input, lines, self, settings;
+
+  //   var defaults = {
+  //     input: {
+  //       class: "editor-input",
+  //       contenteditable: "true"
+  //     },
+  //     lines: {
+  //       class: "editor-lines"
+  //     }
+  //   };
+
+  //   var directFocus = function() {
+  //     input.focus();
+  //   };
+
+  //   var inputChanged = function( e ) {
+
+  //   };
+
+  //   var bindEvents = function() {
+  //     self.on("click", directFocus);
+  //     input.on("keypress", inputChanged);
+  //   };
+
+  //   var init = function( options ) {
+  //     settings = $.extend( true, {}, defaults, options );
+
+  //     self = $( "#js-text-editor" );
+
+  //     if( !settings.lines.hidden ) {
+  //       lines = $( "<div />" )
+  //       .attr( settings.lines )
+  //       .appendTo( self );
+  //     }
+
+  //     input = $( "<div />" )
+  //     .attr( settings.input )
+  //     .appendTo( self );
+
+  //     bindEvents();
+  //   };
+
+  //   return {
+  //     init: init
+  //   };
+
+  // })( jQuery );
+
+
+  // Editor.init();
