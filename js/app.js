30bc4f427c652fd4671041d8b2037547454ed3eb
William A. Burgess <silvusvalentine@hotmail.com>
2015-05-12 07:06:48 -0400

    Created a Lexer module

--- js/app.js
+++ js/app.js
@@ -1,38 +1,30 @@
 jQuery(document).ready(function() {
 
-  var dictionary = {
-    entry: {
-      language: "javascript",
-      rules: [{
-        name: "js-string",
-        pattern: RegExp(/((?:["'])(?:(?=(?:\\?))(?:\\?).)*?(?:["']))/g)
-      }, {
-        name: "js-keyword",
-        pattern: RegExp(/((?:\b)(?:abstract|await|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|export|extends|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|transient|try|typeof|var|void|volatile|while|with|yield)(?=(?:\b)))/g)
-      }, {
-        name: "js-operator",
-        pattern: RegExp(/((?:\+|\-|\*|\/|\%|\>|<|\!|\&|\||\^|\~|\=))/g),
-      }, {
-        name: "js-literal",
-        pattern: RegExp("((?:\\b)(?:null|true|false)(?=(?:\\b)))", "g")
-      }, {
-        name: "js-number",
-        pattern: RegExp(/([-+]?[0-9]*\.?[0-9]+(?:[eE][-+]?[0-9]+)?)/g)
-      }, {
-        name: "js-method",
-        pattern: RegExp(/((?:\.)(?:[\w]+)(?:\())/g)
-      }, {
-        name: "whitespace",
-        pattern: RegExp(/([\s])/g)
-      }]
-    },
+  var options = {
+    language: "javascript",
+    rules: [{
+      name: "js-string",
+      pattern: RegExp(/((?:["'])(?:(?=(?:\\?))(?:\\?).)*?(?:["']))/g)
+    }, {
+      name: "js-keyword",
+      pattern: RegExp(/((?:\b)(?:abstract|await|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|export|extends|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|transient|try|typeof|var|void|volatile|while|with|yield)(?=(?:\b)))/g)
+    }, {
+      name: "js-operator",
+      pattern: RegExp(/((?:\+|\-|\*|\/|\%|\>|<|\!|\&|\||\^|\~|\=))/g),
+    }, {
+      name: "js-literal",
+      pattern: RegExp(/((?:\\b)(?:null|true|false)(?=(?:\\b)))/g)
+    }, {
+      name: "js-number",
+      pattern: RegExp(/([-+]?[0-9]*\.?[0-9]+(?:[eE][-+]?[0-9]+)?)/g)
+    }, {
+      name: "js-method",
+      pattern: RegExp(/((?:\.)(?:[\w]+)(?=\())/g)
+    }, {
+      name: "whitespace",
+      pattern: RegExp(/([\s])/g)
+    }]
   };
-
-  var test = RegExp(dictionary.entry.rules.map(function(elem) {
-    return elem.pattern.source;
-  }).join("|"), "g");
-
-  console.log(test);
 
   var input = $(".input-field"),
     output = document.querySelector(".output-field"),
@@ -57,14 +49,6 @@ jQuery(document).ready(function() {
 
     var textSplit = text.split(/\r?\n/g);
 
-    function printmatches(matches) {
-      for (var i = 0; i < dictionary.entry.rules.length; i++) {
-        if (matches[i + 1] !== undefined) {
-          console.log("match: " + matches[i + 1] + " type: " + dictionary.entry.rules[i].name);
-        }
-      }
-    }
-
     output.innerHTML = "";
 
     textSplit.forEach(function(item, index) {
@@ -73,14 +57,11 @@ jQuery(document).ready(function() {
       line.setAttribute("class", "output-line");
 
       if (item) {
-        console.log("=======newline=======");
-        var matches;
-        while ((matches = test.exec(item))) {
-          var match = matches.input.substring(matches.index, test.lastIndex);
-          //console.log("m: " + match + " start: " + matches.index + " end: " + test.lastIndex);
-          console.log(matches);
-          printmatches(matches);
+        var testToken = Lexer.tokenize(item);
+        for (var i = 0; i < testToken.length; i++) {
+          console.log("type: " + testToken[i].type + " content: " + testToken[i].content);
         }
+        line.innerHTML = item;
       } else {
         line.innerHTML = " ";
       }
@@ -98,64 +79,78 @@ jQuery(document).ready(function() {
     $(e.currentTarget).removeClass("active");
   }
 
+
+
+
+
+
+  var Lexer = (function(options) {
+
+    var language = options.language;
+
+    var rules = options.rules;
+
+    // The regex variable is a RegExp object that combines all
+    // of the individual patterns from the rules object as
+    // capturing groups within a single RegExp pattern.
+
+    var regex = RegExp(rules.map(function(rule) {
+      return rule.pattern.source;
+    }).join("|"), "g");
+
+    var determineType = function(match) {
+
+      // For each capturing group in the RegExp object, determine
+      // which group was captured by the match, and return the
+      // rule's name based on the capturing group's index.
+
+      for (var i = 0; i < rules.length; i++) {
+
+        // Since match[0] will always return the full match of the
+        // RegExp object's exec() function, the capturing group
+        // will be the first defined value after match[0].
+
+        if (match[i + 1] !== undefined) {
+          return rules[i].name;
+        }
+      }
+    };
+
+    var tokenize = function(text) {
+      var match,
+        previousIndex = 0,
+        tokens = [];
+
+      while ((match = regex.exec(text))) {
+
+        // If the match.index is not the same as the previousIndex
+        // there is a range of unmatched characters, which will
+        // be stored in the tokens array with type unknown.
+
+        if (match.index != previousIndex) {
+          tokens.push({
+            type: "unknown",
+            content: text.substring(previousIndex, match.index)
+          });
+        }
+
+        tokens.push({
+          type: determineType(match),
+          content: match[0]
+        });
+
+        previousIndex = regex.lastIndex;
+
+      }
+
+      return tokens;
+
+    };
+
+    return {
+      parse: parse
+    };
+
+  })(options);
+
 });
-
-// strings - (["'])(?:(?=(\\?))\2.)*?\1
-
-// var floats = /[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/g;
-
-// // matches .log(
-// var reMethod = /\.[a-zA-Z]+\(/g;
-// text.replace(reMethod, methodReplacer("$&"));
-
-// function methodReplacer(text) {
-//   var methodName = text.replace(/[\.\(]/, "");
-// }
-
-
-// var Editor = (function($) {
-
-//     var self, settings;
-
-//     var defaults = {
-//       self: {
-//         class: ".js-text-editor"
-//       }
-//     };
-
-//     var elms = [
-//       "editor-container",
-//         "editor-header",
-//         "editor-content",
-//           "input-container",
-//             "input-header",
-//             "input-content",
-//               "input-field",
-//           "output-container",
-//             "output-header",
-//             "output-content",
-//               "output-gutter",
-//               "output-field"
-//     ];
-
-//     var bindEvents = function() {
-
-//     };
-
-//     var init = function(options) {
-//       settings = $.extend(true, {}, defaults, options);
-
-//       self = $(settings.self.class);
-//       console.log(self);
-
-//       bindEvents();
-//     };
-
-//     return {
-//       init: init
-//     };
-
-//   })(jQuery);
-
-
-//   Editor.init();
